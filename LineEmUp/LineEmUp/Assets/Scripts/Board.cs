dfbaec269bbd8912de6a8e1e8d7b4c9519041b19
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-04-16 19:21:21 -0700

    Update Board.cs

--- LineEmUp/LineEmUp/Assets/Scripts/Board.cs
+++ LineEmUp/LineEmUp/Assets/Scripts/Board.cs
@@ -44,14 +44,55 @@ public class Board
 
     }
 
-    public void ProtectAdjacent()
+    public void ProtectAdjacent(int row, int col)
     {
-        
+        grid[row, col].isProtected = true;
     }
 
-    public void PushRow()
+    public void PushRow(int row, int col, bool left)
     {
+        PlaceCoin(row, col);
 
+        if (!left)
+        {
+            int upperBound = 9;
+            for (int horizontalPos = col; horizontalPos < 9; horizontalPos++)
+            {
+                if (grid[row, horizontalPos].isProtected) upperBound = horizontalPos;
+                grid[row, horizontalPos].isProtected = false;
+            }
+
+            for (int horizontalPos = upperBound - 1; horizontalPos > col; horizontalPos++)
+            {
+                grid[row, horizontalPos] = grid[row, horizontalPos - 1];
+            }
+            if (col != 8)
+            {
+                grid[row, col + 1] = null;
+                //add falling
+            }
+
+
+        }
+
+        else
+        {
+            int lowerBound = 0;
+            for (int horizontalPos = col; lowerBound > 0; horizontalPos++)
+            {
+                if (grid[row, horizontalPos].isProtected) lowerBound = horizontalPos;
+                grid[row, horizontalPos].isProtected = false;
+            }
+
+            for (int horizontalPos = lowerBound; horizontalPos < col; horizontalPos++)
+            {
+                grid[row, horizontalPos] = grid[row, horizontalPos + 1];
+            }
+            if (col != 0)
+            {
+                grid[row, col - 1] = null;
+            }
+        }
     }
     #endregion
 }

2fe773f15c593922263f30f61ebbc2866280279e
myong003 <matthew.yong0801@gmail.com>
2022-05-11 16:00:11 -0700

    fixed camera movement

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -6,6 +6,7 @@ public class CameraMove : MonoBehaviour
 {
     [SerializeField] private Vector3 player1Pos;
     [SerializeField] private Vector3 player2Pos;
+    [SerializeField] private Vector3 middlePos;
     public float transitionSpeed;
     public float rotationSpeed;
     private int currPlayer = 1;
@@ -22,28 +23,37 @@ public class CameraMove : MonoBehaviour
         
         Vector3 currPos = gameObject.transform.position;
         Quaternion currRot = gameObject.transform.rotation;
-        Vector3 nextPos;
-        Quaternion nextRot;
+        Vector3 nextPos = middlePos;
+        Quaternion nextRot = Quaternion.Euler(0, 0, 0);
 
-        int xDir;
-        float initZ = gameObject.transform.position.z;
+        while (currPos != nextPos){
+            Vector3 newPos = Vector3.MoveTowards(currPos, nextPos, transitionSpeed * Time.deltaTime);
+
+            Quaternion newRot = Quaternion.Slerp(currRot, nextRot, rotationSpeed * Time.deltaTime);
+
+
+            currPos = newPos;
+            currRot = newRot;
+            gameObject.transform.position = newPos;
+            gameObject.transform.rotation = newRot;
+            yield return null;
+        }
+
+
         if (currPlayer == 1){
             nextPos = player2Pos;
             currPlayer = 2;
             nextRot = Quaternion.Euler(0, -90, 0);
-            xDir = 1;
         }
         else{
             nextPos = player1Pos;
             currPlayer = 1;
             nextRot = Quaternion.Euler(0, 90, 0);
-            xDir = -1;
         }
 
         while (currPos != nextPos || currRot != nextRot){
             Vector3 newPos = Vector3.MoveTowards(currPos, nextPos, transitionSpeed * Time.deltaTime);
 
-            // Vector3 newPos = new Vector3(newX, currPos.y, currPos.z);
             Quaternion newRot = Quaternion.Slerp(currRot, nextRot, rotationSpeed * Time.deltaTime);
 
 
